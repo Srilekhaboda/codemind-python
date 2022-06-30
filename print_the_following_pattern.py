c8a086e825a95590b3f0fe6ef28fa6390a76abed
Srilekhaboda <97424801+Srilekhaboda@users.noreply.github.com>
2022-06-30 18:10:43 +0530

    n=int(input()) m=0 for i in range(1,n+1,1):     for j in range(1,n-1,1):         print(j,end="")         if j==n-2:             m=j             for k in range(m-1,0,-1):                 print(k,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,10 @@
 n=int(input())
-for i in range(65,65+n,1):
-    for j in range(1,n+1,1):
-        print(chr(i),end=" ")
+m=0
+for i in range(1,n+1,1):
+    for j in range(1,n-1,1):
+        print(j,end="")
+        if j==n-2:
+            m=j
+            for k in range(m-1,0,-1):
+                print(k,end="")
     print()
